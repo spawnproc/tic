ea083ecc88b2782c0f867a8858990c335d02b426
Namdak Tonpa <maxim@synrc.com>
2017-02-24 01:18:04 +0200

    bitmex + gdax

---
+++ src/bitmex.erl
@@ -0,0 +1,13 @@
+-module(bitmex).
+-behaviour(websocket_client_handler).
+-export([init/2,websocket_handle/3,websocket_info/3,websocket_terminate/3]).
+
+init([], _)                             -> {ok, 1, 100}.
+websocket_info(start, _, State)         -> {reply, <<>>, State}.
+websocket_terminate(_, _, _)            -> ok.
+websocket_handle({pong, _}, _, State)   -> {ok, State};
+websocket_handle({text, Msg}, _, State) -> print(Msg), {ok, state(State)};
+websocket_handle(Msg, _Conn, State)     -> print(Msg), {noreply, State}.
+
+state(State) -> State + 1.
+print(Msg)   -> io:format("Received msg ~p~n", [Msg]).
--- src/trade.erl
+++ src/trade.erl
@@ -4,9 +4,8 @@
 -behaviour(application).
 -export([start/2, stop/1, init/1]).
 
-tables()   -> [ cookies, actions, globals, caching ].
-opt()      -> [ set, named_table, { keypos, 1 }, public ].
-start(_,_) -> websocket_client:start_link("wss://www.bitmex.com/realtime?subscribe=trade,orderBookL2:XBTUSD", client, []).
+start(_,_) -> lists:foldl(fun({B,A},_) -> websocket_client:start_link(A, B, []) end, ignore, venues()).
 stop(_)    -> ok.
-init([])   -> [ ets:new(T,opt()) || T <- tables() ],
-              { ok, { { one_for_one, 5, 10 }, [] } }.
+init([])   -> { ok, { { one_for_one, 5, 10 }, [] } }.
+venues()   -> [{gdax,   "wss://ws-feed.gdax.com"},
+               {bitmex, "wss://www.bitmex.com/realtime?subscribe=trade,orderBookL2:XBTUSD"}].
