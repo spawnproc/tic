f2217fc8f0c7110caeaee5e8fc6edcff9860393b
Namdak Tonpa <maxim@synrc.com>
2017-02-23 23:34:22 +0200

    can't cast to websocket

--- src/client.erl
+++ src/client.erl
@@ -2,25 +2,15 @@
 -behaviour(websocket_client_handler).
 -export([init/2,websocket_handle/3,websocket_info/3,websocket_terminate/3]).
 
-init([], _ConnState) ->
-    websocket_client:cast(self(), {text, <<"\"op\": \"subscribe\", \"args\": [\"trade\"]}">>}),
-    {ok, 2, 1000}.
-
-websocket_handle({pong, _Msg}, _ConnState, State) ->
-    io:format("Received pong ~n"),
-    Proto = websocket_req:protocol(_ConnState),
-    {ok, State};
-websocket_handle({text, Msg}, _ConnState, 5) ->
-    io:format("Received msg ~p~n", [Msg]),
-    {close, <<>>, 10};
+init([], _ConnState) -> {ok, 1, 1000}.
+websocket_info(start, _ConnState, State) -> {reply, <<>>, State}.
+websocket_terminate(Reason, _ConnState, State) -> ok.
+websocket_handle({pong, _Msg}, _ConnState, State) -> {ok, State};
 websocket_handle({text, Msg}, _ConnState, State) ->
     io:format("Received msg ~p~n", [Msg]),
-    {noreply, State + 1}.
-
-websocket_info(start, _ConnState, State) ->
-    {reply, {text, <<"erlang message received">>}, State}.
-
-websocket_terminate(Reason, _ConnState, State) ->
-    io:format("Websocket closed in state ~p wih reason ~p~n",
-              [State, Reason]),
-    ok.
+    case State of
+         1 -> {reply, {text, <<"{\"op\": \"subscribe\", \"args\": [\"trade\"]}">>},      state(State)};
+         2 -> {reply, {text, <<"{\"op\": \"subscribe\", \"args\": [\"insurance\"]}">>},  state(State)};
+         3 -> {reply, {text, <<"{\"op\": \"subscribe\", \"args\": [\"instrument\"]}">>}, state(State)};
+         _ -> {noreply, State + 1} end.
+state(State) -> State + 1.
--- src/trade.erl
+++ src/trade.erl
@@ -6,7 +6,7 @@
 
 tables()   -> [ cookies, actions, globals, caching ].
 opt()      -> [ set, named_table, { keypos, 1 }, public ].
-start(_,_) -> websocket_client:start_link("wss://www.bitmex.com/realtime", client, []).
+start(_,_) -> websocket_client:start_link("wss://www.bitmex.com/realtime?subscribe=instrument,trade,insurance", client, []).
 stop(_)    -> ok.
 init([])   -> [ ets:new(T,opt()) || T <- tables() ],
               { ok, { { one_for_one, 5, 10 }, [] } }.
