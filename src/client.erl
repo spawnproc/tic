c1379a539e8d01197a5a20d4d17f7b17a593ec62
Namdak Tonpa <maxim@synrc.com>
2017-02-24 00:58:37 +0200

    listen the strean

--- src/client.erl
+++ src/client.erl
@@ -2,15 +2,12 @@
 -behaviour(websocket_client_handler).
 -export([init/2,websocket_handle/3,websocket_info/3,websocket_terminate/3]).
 
-init([], _ConnState) -> {ok, 1, 1000}.
-websocket_info(start, _ConnState, State) -> {reply, <<>>, State}.
-websocket_terminate(Reason, _ConnState, State) -> ok.
-websocket_handle({pong, _Msg}, _ConnState, State) -> {ok, State};
-websocket_handle({text, Msg}, _ConnState, State) ->
-    io:format("Received msg ~p~n", [Msg]),
-    case State of
-         1 -> {reply, {text, <<"{\"op\": \"subscribe\", \"args\": [\"trade\"]}">>},      state(State)};
-         2 -> {reply, {text, <<"{\"op\": \"subscribe\", \"args\": [\"insurance\"]}">>},  state(State)};
-         3 -> {reply, {text, <<"{\"op\": \"subscribe\", \"args\": [\"instrument\"]}">>}, state(State)};
-         _ -> {noreply, State + 1} end.
+init([], _)                             -> {ok, 1, 100}.
+websocket_info(start, _, State)         -> {reply, <<>>, State}.
+websocket_terminate(_, _, _)            -> ok.
+websocket_handle({pong, _}, _, State)   -> {ok, State};
+websocket_handle({text, Msg}, _, State) -> print(Msg), {ok, state(State)};
+websocket_handle(Msg, _Conn, State)     -> print(Msg), {noreply, State}.
+
 state(State) -> State + 1.
+print(Msg)   -> io:format("Received msg ~p~n", [Msg]).
