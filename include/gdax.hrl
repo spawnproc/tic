2e4a02fe4972299be9602df049ed7231522db151
Namdak Tonpa <maxim@synrc.com>
2017-02-24 05:02:41 +0200

    hrl

--- include/gdax.hrl
+++ include/gdax.hrl
@@ -1,13 +1,5 @@
 
--record(gdax, { type=[],
-                  order_id=[],
-                  order_type=[],
-                  size=[],
-                  client_oid=[],
-                  price=[],
-                  side=[],
-                  reason=[],
-                  remaining_size=[],
-                  product_id=[],
-                  sequence=[],
-                  time=[] }).
+-record(gdax, { type=[], order_id=[], order_type=[],
+                size=[], client_oid=[], price=[],
+                side=[], reason=[], remaining_size=[],
+                product_id=[], sequence=[], time=[] }).
