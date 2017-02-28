7c2b8c87b100c2d2d0ca3352299f1641492907fe
Namdak Tonpa <maxim@synrc.com>
2017-02-28 04:21:01 +0200

    cleanup

--- src/sym.erl
+++ src/sym.erl
@@ -6,9 +6,6 @@
 -export([post/2]).
 -rest_record(sym).
 
-instance() -> #sym{}.
-post({Data},_) -> from_json(Data, instance()).
-
 f(T,[])          -> [];
 f(T,[A])         -> io_lib:format("~s -~p 0~n",    [timestamp(),A]);
 f(T,[trade,P,S]) -> io_lib:format("~s ~s ~s~n",    [timestamp(),  trade:print_float(P),trade:print_float(S)]);
@@ -16,9 +13,10 @@ f(T,[A,P,S,ask]) -> io_lib:format("~s -~p ~s ~s~n",[timestamp(),A,trade:print_fl
 f(T,[A,P,S,bid]) -> io_lib:format("~s +~p ~s ~s~n",[timestamp(),A,trade:print_float(P),trade:print_float(S)]);
 f(T,X)           -> io_lib:format("~s ~p~n",       [timestamp(),X]).
 
-pad(I,X) -> string:left(integer_to_list(I),X,$0).
-
-timestamp() ->
+pad(I,X)         -> string:left(integer_to_list(I),X,$0).
+instance()       -> #sym{}.
+post({Data},_)   -> from_json(Data, instance()).
+timestamp()      ->
     {Ma,Sa,Micro} = os:timestamp(),
     {_,{A,B,C}} = calendar:seconds_to_daystime(Ma*1000000 + Sa),
     io_lib:format("~s:~s:~s.~s",[pad(A,2),pad(B,2),pad(C,2),pad(Micro div 1000,3)]).
