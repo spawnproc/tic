188045793e5cef4ea5a4295030c78ae4bc3d2120
Namdak Tonpa <maxim@synrc.com>
2017-03-03 07:25:20 +0200

    left/right cut of the stream with a given order book

--- src/venues/shot.erl
+++ src/venues/shot.erl
@@ -17,9 +17,11 @@ ret({ok,{{_,C,_},_,A}}) when C>=200, C<300 -> post(jsone:decode(list_to_binary(A
 ret({ok,{S,_,B}})                          -> #shot{};
 ret(Error)                                 -> #shot{}.
 
-cut() -> cut(gdax,'BTC-USD').
-cut(Venue,Topic) -> #shot{sequence=Seq} = shot:get(Topic), Name = Venue:name(Topic),
-            {A,B} = lists:partition(fun(X) -> X#order.sn < Seq end,
-                    lists:filter(fun(#order{sym=Name}) -> true; (_) -> false end,
-                    kvs:all(order))),
-            {{A,length(A)},{B,length(B)}}.
+cut() -> cut(gdax,"ETH-USD").
+cut(Venue,Topic) ->
+    #shot{sequence=Seq} = shot:get(Topic), Name = Venue:name(Topic),
+    {Orders,_} = lists:partition(fun(X) -> X#order.sn =< Seq
+                                   andalso X#order.sym == Name end, kvs:all(order)),
+    Ticks  = lists:map(fun(#order{size=S,price=P,uid=I,side=Side,sym=Sym}=O) ->
+                            #tick{size=-S,price=P,id=I,side=Side,sym=Sym} end, Orders),
+    {Ticks,length(Ticks)}.
